c8f1f93dac4b798863b90e178aa054f5f4d6adc2
Mert Gur <mertgur23@gmail.com>
2015-04-20 00:34:21 +0300

    gui

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/register', function(req, res, next){
+	res.render('register');
+});
+
+router.get('/login', function(req, res, next){
+	res.render('login');
+});
+
+router.get('/ask', function(req, res, next){
+	res.render('ask');
+});
+
+router.get('/question', function(req, res, next){
+	res.render('question');
+});
+
 module.exports = router;
